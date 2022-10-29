dfccc5cf1b516b768afe3025715e69b7ffce11de
Nabeel Ahmed <naabeelahhmed@gmail.com>
2022-10-29 17:12:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log('Hello World!')
+//  function add(a,b){
+//    return a +b;
+//  }
+//  console.log(add(2,3));
+
+//  //console.log(process.argv);
+//    var arg = process.argv.slice(2);
+//    console.log(add(parseInt(arg[0]), parseInt(arg[1])));
+
+const op = require('./opertations');
+console.log(op.add(3,4));
